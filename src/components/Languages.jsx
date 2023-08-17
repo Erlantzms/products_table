c0411155aedb51418a7614d299df5f471393e512
Erlantz Maturana <erlantzms@gmail.com>
2023-08-17 12:55:49 +0200

    translate languages

--- src/components/Languages.jsx
+++ src/components/Languages.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import i18n from '../../src/i18n';
 import FlagEnglish from "../../src/icons/flag-en.svg";
 import FlagSpanish from "../../src/icons/flag-es.svg";
+import { useTranslation } from 'react-i18next'
 
 function Languages() {
 
   const languages = ['es', 'en']
+  const {t} = useTranslation();
   
   const changeLanguage = (lang) => {
     if (lang=== languages[0]) i18n.changeLanguage(languages[0])
@@ -14,8 +16,8 @@ function Languages() {
 
   return (
     <div className="relative inline-flex">
-      <button className='mx-2' onClick={()=>changeLanguage(languages[0])}><img src={FlagSpanish}/></button>
-      <button className='mx-2'onClick={()=>changeLanguage(languages[1])}><img src={FlagEnglish}/></button>
+      <button className='mx-2' onClick={()=>changeLanguage(languages[0])}><img src={FlagSpanish}/>{t("translation.es")}</button>
+      <button className='mx-2'onClick={()=>changeLanguage(languages[1])}><img src={FlagEnglish}/>{t("translation.en")}</button>
     </div>
   );
 }
